7216a1cbbf13a77bf8bd54318becd06c784c061c
Satya Kommineni <shanmugaraja.shanmugaraja@boeing.com>
2023-04-21 19:14:09 +0530

    All Code Changes Api and SP

--- GamingApplication/Controllers/RouletteController.cs
+++ GamingApplication/Controllers/RouletteController.cs
@@ -16,7 +16,6 @@ namespace GamingApplication.Controllers
     {
         private readonly IGamingApplication _gamingFunction;
 
-
         [System.Web.Http.Route("api/game/spin")]
         [System.Web.Http.HttpPost]
         public async Task<HttpResponseMessage> Spin([FromBody] Request Req)
